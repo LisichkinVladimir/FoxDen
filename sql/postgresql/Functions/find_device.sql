ddc4d759fbe2eb0cb6fdcc291764451d7d4257d6
LisichkinVladimir <alisichkin@sportmaster.ru>
2025-11-30 14:51:41 +0300

    доработка метода find_device

--- sql/postgresql/Functions/find_device.sql
+++ sql/postgresql/Functions/find_device.sql
@@ -1,13 +1,18 @@
-create or replace function find_device(p_mac_address varchar, p_pin integer default null) returns integer 
+create or replace function find_device(p_mac_address varchar, p_pin integer default null) RETURNS 
+TABLE
+(
+	id integer, 
+	pin integer
+) 
 as $$
 declare
 	v_result integer;
 begin
-	select id into v_result
-	from public.devices
-	where mac_address = p_mac_address
-	  and pin = coalesce(p_pin, pin)
-	  and state = true;
-	return v_result;  
+	return query
+	select t.id, t.pin
+	from public.devices t
+	where t.mac_address = p_mac_address
+	  and t.pin = coalesce(p_pin, t.pin)
+	  and t.state = true;
 end;
 $$ language plpgsql SECURITY DEFINER;
