8e3c83e873bf0be26778f895fdd0c56c6a0ae364
LisichkinVladimir <aleksandr.lisichkin@aerosibs.ru>
2026-03-03 19:11:19 +0300

    Исправление ошибок

--- sql/postgresql/Grants/grants.sql
+++ sql/postgresql/Grants/grants.sql
@@ -2,5 +2,6 @@ GRANT EXECUTE ON FUNCTION public.find_device(character varying, integer) TO foxd
 GRANT EXECUTE ON FUNCTION public.get_user(character varying, character varying) TO foxden_user;
 GRANT EXECUTE ON FUNCTION public.get_device_changes(integer) TO foxden_user;
 GRANT EXECUTE ON PROCEDURE public.add_device_changes(character varying, integer, timestamp with time zone) TO foxden_user;
+GRANT SELECT ON TABLE public.users TO foxden_user;
 GRANT SELECT ON TABLE public.roles TO foxden_user;
 GRANT SELECT ON TABLE public.user_roles TO foxden_user;
